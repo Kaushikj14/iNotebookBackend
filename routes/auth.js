97e89bc508848dc2d632be88922494ce9cccd773
Kaushikj14 <kaushikjain1423@gmail.com>
2024-02-11 18:03:14 +0530

    User login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'harryisagoodb&oy';
 
 // create a user using POST "/api/auth/createuser".Doesn't require authentication(No login required)
 router.post('/createuser', [
@@ -23,17 +27,71 @@ router.post('/createuser', [
             return res.status(400).json({ errors: "Sorry user already exists with same email ID" })
         }
         else {
+            // secured password
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password,salt);
+            // create a new user
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email,
             });
         }
-        res.json(user)
+
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        // console.log(jwtdata);
+        res.json({authToken})
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Soome error occured")
     }
+});
+
+// Authenticate a user .No login required
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),    
+    body('password','Enter a password').exists(),    
+],async (req,res)=>{
+    // If there are errors then send the bad request and error message
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+
+
+        // bcrypt.compare is a async function so we have to add await else we will get the error
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+
+        // data of the user which we will be sendings
+        const payload = {
+            user:{
+                id:user.id,
+            }
+        }
+        
+        const authToken = jwt.sign(payload,JWT_SECRET);
+        res.json({authToken})
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server  error occured");
+    }
+
+
 });
 
 
