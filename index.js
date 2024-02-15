b8917bc9e4dc3c3f17c7d409e1fbba0c46b58227
Kaushikj14 <kaushikjain1423@gmail.com>
2024-02-15 23:02:44 +0530

    final changes

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.use('/api/notes',require('./routes/notes'));
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebbok app listening on port ${port}`)
 })
